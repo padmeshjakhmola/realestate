7c06ec825d086e748871f1176ab2834f1e511091
Padmesh Jakhmola <prankshadow@gmail.com>
2025-05-20 20:10:48 +0530

    feat: homepage

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,8 +1,10 @@
 import { settings } from "@/constants/data";
 import icons from "@/constants/icons";
-import images from "@/constants/images";
+import { logout } from "@/lib/appwrite";
+import { useGlobalContext } from "@/lib/global-provider";
 import React from "react";
 import {
+  Alert,
   Image,
   ImageSourcePropType,
   ScrollView,
@@ -42,7 +44,18 @@ const SettingsItem = ({
 );
 
 const Profile = () => {
-  const handleLogout = async () => {};
+  const { user, refetch } = useGlobalContext();
+
+  const handleLogout = async () => {
+    const result = await logout();
+
+    if (result) {
+      Alert.alert("Success", "You have been logged out");
+      refetch();
+    } else {
+      Alert.alert("Error", "An error occured while logging out");
+    }
+  };
   return (
     <SafeAreaView className="h-full bg-white">
       <ScrollView
@@ -57,14 +70,14 @@ const Profile = () => {
         <View className="flex-row justify-center flex mt-5">
           <View className="flex flex-col items-center relative mt-5">
             <Image
-              source={images.avatar}
+              source={{ uri: user?.avatar }}
               className="size-44 relative rounded-full"
             />
             <TouchableOpacity className="absolute bottom-11 right-2">
               <Image source={icons.edit} className="size-9" />
             </TouchableOpacity>
 
-            <Text className="text-2xl font-rubik-bold mt-2">Prank Shadow</Text>
+            <Text className="text-2xl font-rubik-bold mt-2">{user?.name}</Text>
           </View>
         </View>
 
